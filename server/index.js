506920040fdc0f8803b1605f60c8a0309ce3524e
Dick Kreisberg <rbkreisberg@gmail.com>
2014-06-06 15:55:26 -0700

    reorganize code

--- server/index.js
+++ server/index.js
@@ -3,16 +3,16 @@ var browserify = require('browserify-middleware');
 var forwarded = require('forwarded-for');
 var debug = require('debug');
 
-process.title = 'weplay-io';
+process.title = 'scorch-io';
 
-var port = process.env.WEPLAY_PORT || 3001;
+var port = process.env.SCORCH_PORT || 3001;
 var io = module.exports = sio(port);
 console.log('listening on *:' + port);
 
-var throttle = process.env.WEPLAY_IP_THROTTLE || 100;
+var throttle = process.env.SCORCH_IP_THROTTLE || 100;
 
 // redis socket.io adapter
-var uri = process.env.WEPLAY_REDIS || 'localhost:6379';
+var uri = process.env.SCORCH_REDIS || 'localhost:6379';
 io.adapter(require('socket.io-redis')(uri));
 
 // redis queries instance
@@ -29,7 +29,7 @@ var keys = {
   start: 7
 };
 
-var uid = process.env.WEPLAY_SERVER_UID || port;
+var uid = process.env.SCORCH_SERVER_UID || port;
 debug('server uid %s', uid);
 
 io.total = 0;
@@ -45,7 +45,7 @@ io.on('connection', function(socket){
   });
 
   // send events log so far
-  redis.lrange('weplay:log', 0, 20, function(err, log){
+  redis.lrange('scorch:log', 0, 20, function(err, log){
     if (!Array.isArray(log)) return;
     log.reverse().forEach(function(data){
       data = data.toString();
@@ -56,15 +56,15 @@ io.on('connection', function(socket){
   // broadcast moves, throttling them first
   socket.on('move', function(key){
     if (null == keys[key]) return;
-    redis.get('weplay:move-last:' + ip, function(err, last){
+    redis.get('scorch:move-last:' + ip, function(err, last){
       if (last) {
         last = last.toString();
         if (Date.now() - last < throttle) {
           return;
         }
       }
-      redis.set('weplay:move-last:' + ip, Date.now());
-      redis.publish('weplay:move', keys[key]);
+      redis.set('scorch:move-last:' + ip, Date.now());
+      redis.publish('scorch:move', keys[key]);
       socket.emit('move', key, socket.nick);
       broadcast(socket, 'move', key, socket.nick);
     });
@@ -87,14 +87,14 @@ io.on('connection', function(socket){
 // sends connections count to everyone
 // by aggregating all servers
 function updateCount(total){
-  redis.hset('weplay:connections', uid, total);
+  redis.hset('scorch:connections', uid, total);
 }
 
 // broadcast events and persist them to redis
 
 function broadcast(socket/*, …*/){
   var args = Array.prototype.slice.call(arguments, 1);
-  redis.lpush('weplay:log', JSON.stringify(args));
-  redis.ltrim('weplay:log', 0, 20);
+  redis.lpush('scorch:log', JSON.stringify(args));
+  redis.ltrim('scorch:log', 0, 20);
   socket.broadcast.emit.apply(socket, args);
 }
